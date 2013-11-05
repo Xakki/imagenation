255d12458115951db70f8db16e2b678b5ea643a2
Xakki <xakki@ya.ru>
2013-11-05 14:14:21 +0400

    Initialize imagenation lib

---
+++ Imagenation.php
@@ -0,0 +1,296 @@
+<?php
+/**
+ * Class Imagenation
+ * Реализованно для GD2 , Imagick PHP и ImageMagick
+ * @author Xakki
+ */
+class Imagenation
+{
+    public static $filemod = 0764;
+
+    public static $quality = 80;
+
+    /**
+     * GD2
+     * Imagick
+     * ImageMagick cmd - very fast method
+     */
+    public static $method = 'GD2';
+
+    /**
+     * Меняет размер обрезая
+     * @param $InFile
+     * @param $OutFile
+     * @param $WidthX
+     * @param $HeightY
+     * @return bool
+     */
+    static function thumbnailImage($InFile, $OutFile, $WidthX, $HeightY)
+    {
+        if (!$WidthX and !$HeightY) {
+            return true;
+        }
+
+        $crop = true;
+        if (!$WidthX) {
+            $WidthX = $HeightY;
+            $crop = false;
+        }
+        if (!$HeightY) {
+            $HeightY = $WidthX;
+            $crop = false;
+        }
+
+        self::_chmod($InFile);
+
+        if (self::$method == 'GD2') {
+            include_once(__DIR__ . '/ImagenationGD2.php');
+            return ImagenationGD2::thumbnailImage($InFile, $OutFile, $WidthX, $HeightY);
+        }
+        elseif (self::$method == 'Imagick') {
+            if (class_exists('Imagick', false)) {
+                throw new Exception('Need instal Imagick plugin');
+            }
+            exit('TODO: Imagick');
+            $thumb = new Imagick($InFile);
+            if ($crop) {
+                $thumb->cropThumbnailImage($WidthX, $HeightY);
+            }
+            else {
+                $thumb->thumbnailImage($WidthX, $HeightY, true);
+            }
+            $res = $thumb->writeImage($OutFile);
+            $thumb->destroy();
+        }
+        elseif (self::$method == 'ImageMagick') {
+            if ($crop) {
+                $cmd = 'convert ' . escapeshellarg($InFile) . ' -resize "' . $WidthX . 'x' . $HeightY . '^" -gravity center -crop ' . $WidthX . 'x' . $HeightY . '+0+0 +repage  ' . escapeshellarg($OutFile);
+            }
+            else {
+                $cmd = 'convert ' . escapeshellarg($InFile) . ' -thumbnail "' . $WidthX . 'x' . $HeightY . '" ' . escapeshellarg($OutFile);
+            }
+
+            $out = array();
+            $err = 0;
+            $run = exec($cmd, $out, $err);
+            if ($err) {
+                throw new Exception('Error from ImageMagick: ' . $err);
+            }
+        }
+        else {
+            throw new Exception('Use incorrect method ' . self::$method);
+        }
+
+        self::_chmod($OutFile);
+
+        return true;
+    }
+
+    // обрезает
+    static function cropImage($InFile, $OutFile, $WidthX, $HeightY, $posX = 0, $posY = 0)
+    {
+        if (!$WidthX and !$HeightY) {
+            return true;
+        }
+        if (!$WidthX) $WidthX = '';
+        if (!$HeightY) $HeightY = '';
+
+        self::_chmod($InFile);
+
+        if (self::$method == 'GD2') {
+            include_once(__DIR__ . '/ImagenationGD2.php');
+            return ImagenationGD2::cropImage($InFile, $OutFile, $WidthX, $HeightY, $posX, $posY);
+        }
+        elseif (self::$method == 'Imagick') {
+            if (class_exists('Imagick', false)) {
+                throw new Exception('Need instal Imagick plugin');
+            }
+            $thumb = new Imagick($InFile);
+            $thumb->cropImage($WidthX, $HeightY, $posX, $posY);
+            $res = $thumb->writeImage($OutFile);
+            $thumb->destroy();
+        }
+        elseif (self::$method == 'ImageMagick') {
+            $cmd = 'convert ' . escapeshellarg($InFile) . ' -gravity Center -crop ' . $WidthX . 'x' . $HeightY . '+0 ' . escapeshellarg($OutFile);
+            $out = array();
+            $err = 0;
+            $run = exec($cmd, $out, $err);
+            if ($err) {
+                throw new Exception('Error from ImageMagick: ' . $err);
+            }
+        }
+        else {
+            throw new Exception('Use correct method');
+        }
+
+        self::_chmod($OutFile);
+
+        return true;
+    }
+
+    /**
+     * Наложение водяного знака (маркера)
+     *
+     */
+    static function waterMark($InFile, $OutFile, $logoFile, $posX = 0, $posY = 0)
+    {
+        if (!self::_is_image($logoFile)) { // опред тип файла
+            throw new Exception('File ' . $logoFile . ' is not image');
+        }
+
+        if (!$imtypeIn = self::_is_image($InFile)) { // опред тип файла
+            throw new Exception('File ' . $InFile . ' is not image');
+        }
+
+        $res = true;
+        self::_chmod($InFile);
+        list($width_orig, $height_orig) = getimagesize($InFile); // опред размер
+
+
+        if (self::$method == 'GD2') {
+            include_once(__DIR__ . '/ImagenationGD2.php');
+            return ImagenationGD2::waterMark($InFile, $OutFile, $logoFile, $posX, $posY);
+        }
+        elseif (self::$method == 'Imagick') {
+            if (class_exists('Imagick', false)) {
+                throw new Exception('Need instal Imagick plugin');
+            }
+            if (strpos($posX, '%') !== false)
+                $posX = $width_orig * substr($posX, 0, -1) / 100;
+            if (strpos($posY, '%') !== false)
+                $posY = $height_orig * substr($posY, 0, -1) / 100;
+
+            $thumb = new Imagick($InFile);
+            $logo = new Imagick($logoFile);
+            $thumb->compositeImage($logo, imagick::COMPOSITE_DEFAULT, $posX, $posY);
+            $res = $thumb->writeImage($OutFile);
+            $thumb->destroy();
+        }
+        elseif (self::$method == 'ImageMagick') {
+            //southeast //center
+            //$cmd = 'composite -compose bumpmap -gravity south '.escapeshellarg($InFile).' '.escapeshellarg($logoFile).' '.escapeshellarg($OutFile);
+            $cmd = 'convert ' . escapeshellarg($InFile) . ' -gravity SouthWest -draw "image Over 0,0,0,0 ' . escapeshellarg($logoFile) . '" ' . escapeshellarg($OutFile);
+            $out = array();
+            $err = 0;
+            $run = exec($cmd, $out, $err);
+            if ($err) {
+                throw new Exception('Error from ImageMagick: ' . $err);
+            }
+        }
+        else {
+            throw new Exception('Use correct method');
+        }
+
+
+        self::_chmod($OutFile);
+
+        return true;
+    }
+
+    /*************** Helper function *********************/
+
+    static function _is_image($file)
+    {
+        return exif_imagetype($file);
+    }
+
+    static function _get_ext($file, $include_dot = false)
+    {
+        return image_type_to_extension($file, $include_dot);
+    }
+
+    /**
+     * get image color in RGB format function
+     * @param $imageFile_URL
+     * @param int $numColors
+     * @param int $image_granularity
+     * @return array
+     * @throws Exception
+     */
+    static function getImageColor($imageFile_URL, $numColors = 10, $image_granularity = 5)
+    {
+        $image_granularity = max(1, abs((int)$image_granularity));
+        $colors = array();
+        //find image size
+        $size = getimagesize($imageFile_URL);
+        if ($size === false) {
+            throw new Exception('File ' . $imageFile_URL . ": Unable to get image size data");
+        }
+
+        // open image
+        //$img = @imagecreatefromjpeg($imageFile_URL);
+        $img = ImagenationGD2::_imagecreatefrom($imageFile_URL);
+        if (!$img) {
+            throw new Exception('File ' . $imageFile_URL . ": Unable to open image file");
+        }
+
+        // fetch color in RGB format
+        for ($x = 0; $x < $size[0]; $x += $image_granularity) {
+            for ($y = 0; $y < $size[1]; $y += $image_granularity) {
+                $thisColor = imagecolorat($img, $x, $y);
+                $rgb = imagecolorsforindex($img, $thisColor);
+                $red = round(round(($rgb['red'] / 0x33)) * 0x33);
+                $green = round(round(($rgb['green'] / 0x33)) * 0x33);
+                $blue = round(round(($rgb['blue'] / 0x33)) * 0x33);
+                $thisRGB = sprintf('%02X%02X%02X', $red, $green, $blue);
+                if (array_key_exists($thisRGB, $colors)) {
+                    $colors[$thisRGB]++;
+                }
+                else {
+                    $colors[$thisRGB] = 1;
+                }
+            }
+        }
+        arsort($colors);
+        // returns maximum used color of image format like #C0C0C0.
+        return array_slice(($colors), 0, $numColors, true);
+    }
+
+    /**
+     * RGB-Colorcodes(i.e: 255 0 255) to HEX-Colorcodes (i.e: FF00FF)
+     * example - print_r(rgb2hex(array(10,255,255)));
+     */
+    static function rgb2hex($rgb)
+    {
+        if (strlen($hex = dechex($rgb)) == 1) {
+            $hex = "0" . $hex;
+        }
+        return $hex;
+    }
+
+    /**
+     * html(HEX) color to convert in RGB format color like R(255) G(255) B(255)
+     */
+    static function getHtml2Rgb($str_color)
+    {
+        if ($str_color[0] == '#')
+            $str_color = substr($str_color, 1);
+
+        if (strlen($str_color) == 6)
+            list($r, $g, $b) = array($str_color[0] . $str_color[1],
+                $str_color[2] . $str_color[3],
+                $str_color[4] . $str_color[5]);
+        elseif (strlen($str_color) == 3)
+            list($r, $g, $b) = array($str_color[0] . $str_color[0], $str_color[1] . $str_color[1], $str_color[2] . $str_color[2]);
+        else
+            return false;
+
+        $r = hexdec($r);
+        $g = hexdec($g);
+        $b = hexdec($b);
+        $arr_rgb = array($r, $g, $b);
+        // Return colors format liek R(255) G(255) B(255)
+        return $arr_rgb;
+    }
+
+    /*************** TOOLS *******************/
+
+
+    static function _chmod($file)
+    {
+        if (self::$filemod) {
+            chmod($file, self::$filemod);
+        }
+    }
+
+}
---
+++ ImagenationGD2.php
@@ -0,0 +1,307 @@
+<?php
+
+/**
+ * Class ImagenationGD2
+ * Реализованно для GD2
+ * @author Xakki
+ */
+class ImagenationGD2
+{
+
+    // Меняет размер обрезая
+    static function thumbnailImage($InFile, $OutFile, $WidthX, $HeightY)
+    {
+        $trueX = $WidthX;
+        $trueY = $HeightY;
+        Imagenation::_chmod($InFile);
+        list($width_orig, $height_orig) = getimagesize($InFile);
+
+        if (!$trueX and !$trueY) {
+            return true;
+        }
+        if (!$trueX) {
+            $WidthX = $trueX = ($width_orig * $trueY) / $height_orig;
+        }
+        if (!$trueY) {
+            $HeightY = $trueY = ($height_orig * $trueX) / $width_orig;
+        }
+
+        $ratio_orig = $width_orig / $height_orig;
+        if ($WidthX / $HeightY > $ratio_orig) {
+            $HeightY = $WidthX / $ratio_orig;
+        }
+        else {
+            $WidthX = $HeightY * $ratio_orig;
+        }
+        /*Создаем пустое изображение на вывод*/
+        if (!($thumb = @imagecreatetruecolor($WidthX, $HeightY))) {
+            throw new Exception('Cannot Initialize new GD image stream');
+        }
+        /*Определяем тип рисунка*/
+        if (!$imtype = self::_get_type($InFile)) { // опред тип файла
+            throw new Exception('File is not image ' . $InFile);
+        }
+
+        /*Обработка только jpeg, gif, png*/
+        if ($imtype > 3) {
+            trigger_error('Данный тип изображения не поддерживается на данный момент, рекомендуем использовать JPEG, PNG или GIF', E_USER_NOTICE);
+            copy($InFile, $OutFile);
+            return true;
+        }
+
+        /*Открываем исходный рисунок*/
+        if (!$source = self::_imagecreatefrom($InFile, $imtype)) { //открываем рисунок
+            throw new Exception('File ' . $InFile . ' is not image');
+        }
+
+        if (!imagecopyresampled($thumb, $source, 0, 0, 0, 0, $WidthX, $HeightY, $width_orig, $height_orig)) {
+            throw new Exception('Error imagecopyresampled ' . $thumb);
+        }
+
+        if (!($thumb2 = @imagecreatetruecolor($trueX, $trueY))) {
+            throw new Exception('Cannot Initialize new GD image stream');
+        }
+
+        if (!imagecopyresampled($thumb2, $thumb, 0, 0, $WidthX / 2 - $trueX / 2, $HeightY / 2 - $trueY / 2, $trueX, $trueY, $trueX, $trueY)) {
+            throw new Exception('Error imagecopyresampled');
+        }
+
+
+        self::_image_to_file($thumb2, $OutFile, Imagenation::$quality, $imtype); //сохраняем в файл
+
+        if (!file_exists($OutFile)) {
+            throw new Exception('Cant create file');
+        }
+        return true;
+    }
+
+
+    /**
+     * обрезает картинку
+     * @param $InFile
+     * @param $OutFile
+     * @param $WidthX
+     * @param $HeightY
+     * @return bool
+     * @throws Exception
+     */
+    static function cropImage($InFile, $OutFile, $WidthX, $HeightY)
+    {
+        Imagenation::_chmod($InFile);
+        list($width_orig, $height_orig) = getimagesize($InFile); // опред размер
+
+        if (!$WidthX and !$HeightY)
+            return true;
+        if (!$WidthX)
+            $WidthX = ($width_orig * $HeightY) / $height_orig;
+        if (!$HeightY) {
+            $HeightY = ($height_orig * $WidthX) / $width_orig;
+        }
+
+        // Resample
+        $thumb = imagecreatetruecolor($WidthX, $HeightY); //созд пустой рисунок
+        if (!$imtype = self::_get_type($InFile)) { // опред тип файла
+            throw new Exception('File is not image ' . $InFile);
+        }
+        if ($imtype > 3) {
+            trigger_error('Данный тип изображения не поддерживается на данный момент, рекомендуем использовать JPEG, PNG или GIF', E_USER_NOTICE);
+            copy($InFile, $OutFile);
+            return true;
+        }
+        $source = self::_imagecreatefrom($InFile, $imtype); //открываем рисунок
+        if (!$source) {
+            throw new Exception('Unable to open image file ' . $InFile);
+        }
+
+        imagecopyresampled($thumb, $source, 0, 0, $width_orig / 2 - $WidthX / 2, $height_orig / 2 - $HeightY / 2, $WidthX, $HeightY, $WidthX, $HeightY);
+        self::_image_to_file($thumb, $OutFile, Imagenation::$quality, $imtype); //сохраняем в файл
+        if (!file_exists($OutFile)) {
+            throw new Exception('Cant create output file ' . $OutFile);
+        }
+        return true;
+    }
+
+
+    /**
+     * Наложение водяного знака (маркера)
+     *
+     */
+    static function waterMark($InFile, $OutFile, $logoFile, $posX = 0, $posY = 0)
+    {
+        if (!$imtypeIn = self::_get_type($InFile)) { // опред тип файла
+            throw new Exception('File ' . $InFile . ' is not image');
+        }
+
+        if ($imtypeIn > 3) return false;
+
+        if (!$imtypeLogo = self::_get_type($logoFile)) { // опред тип файла
+            throw new Exception('File ' . $logoFile . ' is not image');
+        }
+        if ($imtypeLogo > 3) return false;
+
+        $znak_hw = getimagesize($logoFile);
+        $foto_hw = getimagesize($InFile);
+
+        $znak = self::_imagecreatefrom($logoFile, $imtypeLogo);
+        if (!$znak) {
+            throw new Exception("Unable to open image file " . $logoFile);
+        }
+
+        $foto = self::_imagecreatefrom($InFile, $imtypeIn);
+        if (!$foto) {
+            throw new Exception('error', "Unable to open image file " . $InFile);
+        }
+
+        imagecopy(
+            $foto,
+            $znak,
+            $foto_hw[0] - $znak_hw[0],
+            $foto_hw[1] - $znak_hw[1],
+            0,
+            0,
+            $znak_hw[0],
+            $znak_hw[1]
+        );
+        if (file_exists($OutFile)) {
+            Imagenation::_chmod($OutFile);
+            unlink($OutFile);
+        }
+        self::_image_to_file($foto, $OutFile, Imagenation::$quality, $imtypeIn); //сохраняем в файл
+        imagedestroy($znak);
+        imagedestroy($foto);
+        if (!file_exists($OutFile)) {
+            throw new Exception('Cant save file to ' . $OutFile);
+        }
+        return true;
+    }
+
+    // Меняет размер. пропорционально, до минимального соответсявия по стороне
+    static function _resizeImage($InFile, $OutFile, $WidthX, $HeightY)
+    {
+        Imagenation::_chmod($InFile);
+        list($width_orig, $height_orig) = getimagesize($InFile); // опред размер
+
+        if (!$WidthX and !$HeightY)
+            return true;
+        if (!$WidthX)
+            $WidthX = ($width_orig * $HeightY) / $height_orig;
+        if (!$HeightY) {
+            $HeightY = ($height_orig * $WidthX) / $width_orig;
+        }
+
+        if ($width_orig < $WidthX and $height_orig < $HeightY) {
+            if ($InFile != $OutFile) {
+                copy($InFile, $OutFile);
+                Imagenation::_chmod($OutFile);
+            }
+            return true;
+        }
+        elseif ($width_orig / $WidthX < $height_orig / $HeightY) {
+            $WidthX = round($HeightY * $width_orig / $height_orig);
+        }
+        elseif ($width_orig / $WidthX > $height_orig / $HeightY) {
+            $HeightY = round($WidthX * $height_orig / $width_orig);
+        }
+
+        $thumb = imagecreatetruecolor($WidthX, $HeightY); //созд пустой рисунок
+        if (!$imtype = self::_get_type($InFile)) { // опред тип файла
+            throw new Exception('File ' . $InFile . ' is not image');
+        }
+
+        if ($imtype > 3) {
+            trigger_error('Данный тип изображения не поддерживается на данный момент, рекомендуем использовать JPEG, PNG или GIF', E_USER_NOTICE);
+            copy($InFile, $OutFile);
+            return true;
+        }
+
+        $source = self::_imagecreatefrom($InFile, $imtype); //открываем рисунок
+        if (!$source) {
+            throw new Exception("Unable to open image file " . $InFile);
+        }
+
+        imagecopyresized($thumb, $source, 0, 0, 0, 0, $WidthX, $HeightY, $width_orig, $height_orig); //меняем размер
+        self::_image_to_file($thumb, $OutFile, Imagenation::$quality, $imtype); //сохраняем в файл
+        if (!file_exists($OutFile)) {
+            throw new Exception('Cant create output file ' . $OutFile);
+        }
+        return true;
+    }
+
+    /*************** Helper function *********************/
+
+    static function _imagecreatefrom($im_file, $imtype = null)
+    {
+        if (is_null($imtype)) {
+            $imtype = self::_get_type($im_file);
+        }
+        /*
+Возвращаемое значение	Константа
+1	IMAGETYPE_GIF
+2	IMAGETYPE_JPEG
+3	IMAGETYPE_PNG
+4	IMAGETYPE_SWF
+5	IMAGETYPE_PSD
+6	IMAGETYPE_BMP
+7	IMAGETYPE_TIFF_II
+8	IMAGETYPE_TIFF_MM
+9	IMAGETYPE_JPC
+10	IMAGETYPE_JP2
+11	IMAGETYPE_JPX
+12	IMAGETYPE_JB2
+13	IMAGETYPE_SWC
+14	IMAGETYPE_IFF
+15	IMAGETYPE_WBMP
+16	IMAGETYPE_XBM
+        */
+        if ($imtype == 1) {
+            if (!($image = @imagecreatefromgif($im_file))) {
+                throw new Exception('Can not create a new image from file');
+            }
+        }
+        elseif ($imtype == 2) {
+            if (!($image = imagecreatefromjpeg($im_file))) {
+                throw new Exception('Can not create a new image from file');
+            }
+        }
+        elseif ($imtype == 3) {
+            if (!($image = imagecreatefrompng($im_file))) {
+                throw new Exception('Can not create a new image from file');
+            }
+        }
+        else return false;
+        return $image;
+    }
+
+    static function _image_to_file($im, $file, $q, $imtype)
+    {
+        if ($imtype == 1) {
+            imagegif($im, $file, $q);
+        }
+        elseif ($imtype == 2) {
+            imagejpeg($im, $file, $q);
+        }
+        elseif ($imtype == 3) {
+            imagepng($im, $file, 8);
+        }
+        else {
+            return false;
+        }
+        return true;
+    }
+
+    static function _is_image($file)
+    {
+        $res = exif_imagetype($file);
+        return ($res > 0 && $res < 4);
+    }
+
+    static function _get_type($file)
+    {
+        return exif_imagetype($file);
+    }
+
+    static function _get_ext($file)
+    {
+        return image_type_to_extension($file);
+    }
+}
